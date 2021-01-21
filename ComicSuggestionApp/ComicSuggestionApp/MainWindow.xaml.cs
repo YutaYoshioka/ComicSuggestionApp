267b29764c767e358c17b153a0c14bd23cbe19da
YutaYoshioka <g031s314@s.iwate-pu.ac.jp>
2021-01-22 02:00:27 +0900

    [Update] Beta ver.

--- ComicSuggestionApp/ComicSuggestionApp/MainWindow.xaml.cs
+++ ComicSuggestionApp/ComicSuggestionApp/MainWindow.xaml.cs
@@ -64,7 +64,67 @@ namespace ComicSuggestionApp
 		/// <param name="e"></param>
 		private void suggestButton_Click(object sender, RoutedEventArgs e)
 		{
+			var inputText = inputTextBox.Text;
 
+			var keywordWeight = new double[20] {1, 0.8, 0.64, 0.512, 0.4096, 0.32768, 0.262144, 0.2097152, 0.16777216, 0.134217728, 0.107374182, 0.085899346, 0.068719477, 0.054975581, 0.043980465, 0.035184372, 0.028147498, 0.022517998, 0.018014399, 0.014411519 };
+
+			// 各作品ごとの一致率
+			var totalWeight = new double[comicDB.Length];
+
+			// 作品ごとの一致率を計算
+			for (var i = 0; i < comicDB.Length; i++)
+			{
+				for (var j = 0; j < comicDB[i].keywords.Length; j++)
+				{
+					totalWeight[i] += CountString(inputText, comicDB[i].keywords[j]) * keywordWeight[j];
+				}
+			}
+
+			// 一致度が高い順に最大5作品出力
+
+			// 上位の漫画DB
+			var comicRanking = new List<ComicDB>();
+
+			for (var i = 0; i < 5; i++)
+			{
+				var max = 0.0;
+				var maxIndex = -1;
+
+				for (var j = 0; j < comicDB.Length; j++)
+				{
+					if(max < totalWeight[j])
+					{
+						max = totalWeight[j];
+						maxIndex = j;
+					}
+				}
+
+				if(maxIndex == -1)
+				{
+					break;
+				}
+
+				comicRanking.Add(comicDB[maxIndex]);
+				totalWeight[maxIndex] = -1;
+			}
+
+
+
+			for (int i = 0; i < comicRanking.Count; i++)
+			{
+				resultLabel.Content += (i + 1).ToString() + "位！ " + comicRanking[i].title + "\n";
+			}
+		}
+
+		/// <summary>
+		/// 指定した文字列の中に含まれるキーワードの数をカウントします。
+		/// </summary>
+		/// <param name="target">検索対象文字列</param>
+		/// <param name="keyword">検索キーワード</param>
+		/// <returns>含まれるキーワード数</returns>
+		private static int CountString(string target, string keyword)
+		{
+			return (target.Length - target.Replace(keyword, "").Length) / keyword.Length;
 		}
 
 
@@ -86,7 +146,7 @@ namespace ComicSuggestionApp
 
 			var cDBkeyword = new List<string>();
 
-			for (int i = 0; i < dbStrings.Length; i++)
+			for (var i = 0; i < dbStrings.Length; i++)
 			{
 				// null = 1作品の終わり、DBに登録
 				if(dbStrings[i] == "")
